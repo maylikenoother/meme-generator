67877a9bfd0ee1cb033dc7bffadde9a3658f5cc8
Mary <61589585+maylikenoother@users.noreply.github.com>
2023-06-22 06:06:20 -0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+      <footer className="footer-container">
+        <small className="small">
+          Coded with ❤️ by{" "}
+          <a
+            className="footer-link"
+            href="https://github.com/maylikenoother"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mary Ogedengbe
+          </a>
+          .
+        </small>
+      </footer>
+    );
+  }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import trollFace from '../assets/Troll Face.png'
+
+export default function Header() {
+    return (
+        <header className="header">
+        <img src={trollFace} alt="" className="header-image" />
+        <h2 className="header-title">Meme Generator</h2>
+        <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    )
+}
